29ed1a025f0ef2900b9c7330da6d16254c53e4c9
Ntombi20 <ntombi@projectcodex.co>
2016-04-18 15:14:16 +0200

    got the all the number in the sentence

--- words_numbers.js
+++ words_numbers.js
@@ -1,24 +1,47 @@
-var fs = require('fs');
-
 module.exports = function(filePath){
 
+var fs = require('fs');
+
 this.numbers = function() {
-  var words_numbers = fs.readFileSync(filePath, 'utf8' );
+var words_numbers = fs.readFileSync(filePath, 'utf8' );
+var string = words_numbers
+    .replace("I saw ", "")
+    .replace(" bears, and", "")
+    .replace(" toads in", "")
+    .replace(" meters from the tallest tree in the park.", "")
+    .split(" ");
+//3 bears, and 7 toads in 50 meters from the tallest tree in the park.
 
-  var no = [];
+var num = [];
 
-  var sentence = words_numbers
-      .replace("I saw ", "")
-      .replace(" meters from the tallest tree in the park.", "")
-      .split(" bears, and ");
-      //3 bears, and 7 toads in 50
-      console.log(sentence);
+  string.forEach(function(onlyNum){
+    var newStr = onlyNum
 
-    // sentence.forEach(function(no){
-    //   var str = no.split(" ");
-    //   console.log(str[0]);
-    // });
+    var number = Number(newStr);
 
+    num.push({
+      number: number
+    })
+
+  });
+
+return num;
 }
+
+// this.shorter = function(){
+// var words_numbers = fs.readFileSync(filePath, 'utf8' );
+//
+//   var string = words_numbers.split(" ");
+//
+//   string.forEach(function(words){
+//     console.log(words);
+//
+//   })
+//   //I saw 3 bears, and 7 toads in 50 meters from the tallest tree in the park.
+//
+//
+// }
+
+
 
 };
